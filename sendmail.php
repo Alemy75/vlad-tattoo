05995eb6f128e73f057d1eb2f068aa5b38fb8855
Alemax75 <alemax75962@gmail.com>
2022-10-05 09:07:58 +0300

    Релиз

--- sendmail.php
+++ sendmail.php
@@ -1,49 +1,39 @@
 <?php
-    use PHPmailer\PHPMailer\PHPMailer;
-    use PHPmailer\PHPMailer\Exception;
+if( $_POST ){ 
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
+$name = $_POST['name'];
+$lastName = $_POST['lastName'];
+$phone = $_POST['phone'];
 
-    $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
-    $mail->IsHTML(true);
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-    $mail->setFrom('vldtto@fls.ru', 'Клиент');
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'no-reply-vlad-tattooyar@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '6gA3nVcEgwJkHKXbwP1Y'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-    $mail->addAddress('alemax75962@gmail.com');
+$mail->setFrom('no-reply-vlad-tattooyar@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('vlad-onuchin@mail.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
 
-    $mail->Subject = 'Консультация';
-
-    //  $hand = "Правая";
-    //  if ($_POST['hand'] == "left") {
-    //      $hand = 'Левая';
-    //  }
-
-    $body = '<h1>Письмо с сайта</h1>';
-
-    if(trim(!empty($_POST['name']))){
-        $body.='Имя: '.$_POST['name'].'<br>'; 
-    }
-    if(trim(!empty($_POST['lastName']))){
-        $body.='Фамилия: '.$_POST['lastName'].'<br>'; 
-    }
-    if(trim(!empty($_POST['phone']))){
-        $body.='Телефон: '.$_POST['phone'].'<br>' ;
-    }
-
-    $mail->Body = $body;
-
-    if (!$mail->send()) {
-        $message = 'Ошибка';
-    } else {
-        $message = 'Данные отправлены';
-    }
-
-    $response = ['message' => $message];
-
-    header('Content-type: application/json');
-    echo json_encode($response);
-
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name.' '. $lastName. ' оставил(a) заявку, его телефон ' .$phone;
+$mail->AltBody = '';
+if(!$mail->send()) {
+    echo 'Error';
+};
+}
+?>
 
